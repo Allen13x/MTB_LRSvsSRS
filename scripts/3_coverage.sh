9577f483aff6204224b3a3083a312f070102cc67
allen <dimarco.federico@hsr.it>
2022-08-29 23:16:03 +0200

    220829

--- scripts/3_coverage.sh
+++ scripts/3_coverage.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-mkdir OUTPUT/Coverage
-cd OUTPUT/Coverage
+mkdir Coverage
+cd Coverage
 for j in $(ls -1 reads)
 do
 for i in $(cat reads/$j/sample_list)
 do
-mosdepth -t 8 -x -b ../../REF/mtb_region1k.bed -T 1,2,4,6,8,12,16,20,40 ${i}_1k ../../reads/${j}/Bam/${i}_*.bam
-mosdepth -t 8 -x -b ../../REF/pe_ppe.bed -T 1,2,4,6,8,12,16,20,40 ${i}_pe-ppe ../../reads/${j}/Bam/${i}_*.bam
+mosdepth -t 8 -x -b ../REF/mtb_region1k.bed -T 1,2,4,6,8,12,16,20,40 ${i}_1k ../reads/${j}/Bam/${i}_*.bam
+mosdepth -t 8 -x -b ../REF/pe_ppe.bed -T 1,2,4,6,8,12,16,20,40 ${i}_pe-ppe ../reads/${j}/Bam/${i}_*.bam
 done
 done 
