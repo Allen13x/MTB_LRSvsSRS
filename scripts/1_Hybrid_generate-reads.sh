54189c3bddc3bac55d51364e60de5bd62fa4a5a1
allen <dimarco.federico@hsr.it>
2022-08-30 11:35:17 +0200

    220830

--- scripts/1_Hybrid_generate-reads.sh
+++ scripts/1_Hybrid_generate-reads.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+ls -1 reads/LRS/ | cut -f1 -d '_' > sample_list
+for sample in $(cat sample_list)
+do
 
-Ratatosk -s reads/SRS/${sample}_*R1* -s reads/SRS/${sample}_*R2* -l reads/LRS/${sample}*.fastq.gz -o reads/Hybrid/${sample}
+Ratatosk -s reads/SRS/${sample}_*R1* -s reads/SRS/${sample}_*R2* -l reads/LRS/${sample}_*.fastq.gz -o reads/Hybrid/${sample}_Hybrid.fastq.gz
+
+done
